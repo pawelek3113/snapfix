40b82953418cc023cf0a812e2864504805fc9fcb
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2026-09-07 01:05:22 +0200

    fix: update Python requirement to 3.12 and improve CLI argument descriptions

--- src/snapfix/cli.py
+++ src/snapfix/cli.py
@@ -4,7 +4,7 @@ from snapfix import LoggerBuilder
 import argparse
 from datetime import datetime
 
-def main ():
+def main():
     parser = argparse.ArgumentParser(prog="snapfix", description="A script that fixes datetime metadata tags for snapchat memories and composes main and overlay into a single item.")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
@@ -14,7 +14,7 @@ def main ():
 
     compose_parser = subparsers.add_parser("compose")
     compose_parser.add_argument("root_dir", type=Path, help="Root directory that holds memories (main and overlay files) (they can be nested).")
-    compose_parser.add_argument("output_dir", type=Path, default=Path("./results"))
+    compose_parser.add_argument("output_dir", type=Path, help="Directory that will hold composited items.")
     compose_parser.add_argument("--dry-run", action="store_true", help="Runs the program without affecting your files.")
 
     args = parser.parse_args()
@@ -23,7 +23,7 @@ def main ():
 
     if args.command == "fix-dates":
         with MemoriesFixer(root_dir=args.root_dir, dry_run=args.dry_run, logger=lg) as fixer:
-                fixer.fix_dates()
+            fixer.fix_dates()
     elif args.command == "compose":
             with MemoriesFixer(root_dir=args.root_dir, dry_run=args.dry_run, logger=lg) as fixer:
                 # fixer.compose_all(args.output_dir)

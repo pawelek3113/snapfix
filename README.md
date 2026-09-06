ea080f92250b5230511a21ab2bb75fc6a2f4ed03
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2026-09-07 00:49:22 +0200

    feat: enhance CLI

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Snapfix
 
-A script that fixes time metadata tags for snapchat memories.
+A script that fixes datetime metadata tags for snapchat memories and composes main and overlay into a single item.
+
+___
+
+## Prerequisites
+
+- exiftool
+- poetry
+
+## Installation
+
+```bash
+poetry install
+```
+
+## Usage
+
+```bash
+poetry run snapfix -h
+```
--- src/snapfix/cli.py
+++ src/snapfix/cli.py
@@ -1,23 +1,35 @@
 from snapfix import MemoriesFixer
 from pathlib import Path
 from snapfix import LoggerBuilder
+import argparse
+from datetime import datetime
+
+def main ():
+    parser = argparse.ArgumentParser(prog="snapfix", description="A script that fixes datetime metadata tags for snapchat memories and composes main and overlay into a single item.")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    fix_parser = subparsers.add_parser("fix-dates")
+    fix_parser.add_argument("root_dir", type=Path, help="Root directory that holds memories with their dates to be fixed (they can be nested).")
+    fix_parser.add_argument("-d", "--dry-run", action="store_true", help="Runs the program without affecting your files.")
+
+    compose_parser = subparsers.add_parser("compose")
+    compose_parser.add_argument("root_dir", type=Path, help="Root directory that holds memories (main and overlay files) (they can be nested).")
+    compose_parser.add_argument("output_dir", type=Path, default=Path("./results"))
+    compose_parser.add_argument("--dry-run", action="store_true", help="Runs the program without affecting your files.")
+
+    args = parser.parse_args()
+
+    lg = LoggerBuilder().build(Path(f"memories_fix_{datetime.now().strftime("%Y_%m_%d_%H-%M-%S")}.log"))
+
+    if args.command == "fix-dates":
+        with MemoriesFixer(root_dir=args.root_dir, dry_run=args.dry_run, logger=lg) as fixer:
+                fixer.fix_dates()
+    elif args.command == "compose":
+            with MemoriesFixer(root_dir=args.root_dir, dry_run=args.dry_run, logger=lg) as fixer:
+                # fixer.compose_all(args.output_dir)
+                lg.warning("compose command is not implemented yet")
+                ...
+
 
 if __name__ == "__main__":
-    root_dir = input("""
-    Enter a root directory (covering every Memories package from Snapchat)
-        it should look like that:
-                .
-                ├── 1
-                │   ├── mydata~1774546372096
-                │   └── mydata~1774546372096.zip
-                ├── 2
-                │   ├── mydata~1774546372096-2
-                │   └── mydata~1774546372096-2.zip
-                ├── 3
-                │   ├── mydata~1774546372096-3
-                │   └── mydata~1774546372096-3.zip
-    > """)
-
-    lg = LoggerBuilder().build(Path("memories_fix.log"))
-    with MemoriesFixer(root_dir, dry_run=True, logger=lg) as fixer:
-        fixer.run()
+    main()
